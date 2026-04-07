893271f9b52f4eed74b875e4b32ee2ebe8fc967f
legend0996 <legendmatthew32@gmail.com>
2026-04-07 14:35:50 +0300

    Connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
-
 // attach token if exists
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
